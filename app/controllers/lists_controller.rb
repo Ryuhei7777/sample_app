8c59e8a22ea6bf5d5b8408c1004bc2112207442f
Ryuhei7777 <124151763+Ryuhei7777@users.noreply.github.com>
2023-02-18 04:47:49 +0000

    [Finish]3-8 バリデーション実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,13 +7,16 @@ class ListsController < ApplicationController
   #以下を追加
   def create
      # １.&2. データを受け取り新規登録するためのインスタンス作成
-     list=List.new(list_params)
+     @list=List.new(list_params)
      # 3. データをデータベースに保存するためのsaveメソッド実行
-     list.save
+     if @list.save
      # redirect_to '/top' を削除して、以下コードに変更
      # 詳細画面へリダイレクト
-     redirect_to list_path(list.id)
-
+      redirect_to list_path(list.id)
+    
+     else
+      render :new
+     end
   end
 
   def index
@@ -35,6 +38,11 @@ class ListsController < ApplicationController
     
   end 
   
+  def destroy 
+    list=List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end 
 
   private
   # ストロングパラメータ
